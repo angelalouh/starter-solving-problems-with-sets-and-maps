3bf1dc4f851cd7399c771e40f7c00f841bd205f5
Angela Louh <louh.angela@gmail.com>
2021-11-14 22:23:14 -0800

    Implemented the same letters algorithm

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,25 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  const results = new Map();
+
+  words.forEach((word) => {
+    const set = new Set(word.toLowerCase());
+
+    const arrayOfSetLetters = [...set];
+    const sortedArrayOfLetters = arrayOfSetLetters.sort();
+    const stringOfLetters = sortedArrayOfLetters.join("");
+
+    if (results.get(stringOfLetters)) {
+      const existingArrayValue = results.get(stringOfLetters);
+      results.set(stringOfLetters, [...existingArrayValue, word]);
+    } else {
+      results.set(stringOfLetters, [word]);
+    }
+  });
+
+  return results;
+}
 
 module.exports = sameLetters;
