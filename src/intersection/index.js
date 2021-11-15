6b096daf31fc96846ed45069eecd065cbb2ad077
Angela Louh <louh.angela@gmail.com>
2021-11-14 21:57:14 -0800

    Implemented the array intersection algorithm

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,18 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const map = new Map();
+  const set = new Set();
+
+  a.forEach((element) => {
+    map.set(element, element);
+  });
+
+  b.forEach((element) => {
+    if (map.get(element)) {
+      set.add(element);
+    }
+  });
+
+  return [...set];
+}
 
 module.exports = intersection;
